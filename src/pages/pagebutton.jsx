4c0a4f8a2f40ddf3b4a64a9d73faee70a0ce1c3a
nene <yamada_nene@accelhack.com>
2026-07-02 15:03:12 +0900

    delete unneccesary lines

--- src/pages/pagebutton.jsx
+++ src/pages/pagebutton.jsx
@@ -1,10 +1,6 @@
 import {useState} from 'react';
 
 function PageButton({currentPage, totalPages, setCurrentPage, disabled, onClick, children}){
-    const backPage= ()=>{
-        if(currentPage >1) setCurrentPage(currentPage -1);
-    };
-
     return(
         <button onClick={onClick} disabled= {disabled} style={{display: 'inline-block', margin: '0 5px'}}>
             {children}
